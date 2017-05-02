b2b420669b0ee681ab2dac3cc92f7fee45c0760f
TideDancer <xingyu.develop.cai@gmail.com>
2017-05-01 21:03:55 -0400

    script topk

--- script_topk.m
+++ script_topk.m
@@ -7,18 +7,13 @@ dataname_set{3} = 'ic';
 dataname_set{4} = 'e';
 
 result_root = 'result_topk';
-for i = 1:4
+for i = 1:3
   dataname = dataname_set{i}
   for nfolds = 5:20
     path = strcat(result_root, '/', dataname, '/', num2str(nfolds), '/');
     mkdir(path);
     path
-    sample(nfolds, nruns, dataname);
-
-    copyfile('predictions_all.txt', path);
-    copyfile('predictions_test_all.txt', path);
-    copyfile('label_train_all.txt', path);
-    copyfile('label_test_all.txt', path);
+    sample_topk(nfolds, nruns, dataname, path);
   end
 end
 
